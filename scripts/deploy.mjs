339081aeb35b8c342dbd94f0d157b5fd2d5b5a15
jay4q <jay4qqqq@gmail.com>
2021-12-24 11:48:20 +0800

    fix: 🐛 拷贝环境变量文件至产线代码包

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,19 +1,6 @@
 import { config } from 'dotenv'
 import * as path from 'path'
 
-// 将 devDependencies 删除，因为在云端安装云函数，没有使用 npm install --production 的命令（这个需要腾讯云的负责人去解决）
-function modifyPackageJson() {
-  try {
-    const jsonStr = fs.readFileSync('./package.json', 'utf8')
-    const jsonFile = JSON.parse(jsonStr)
-    delete jsonFile['devDependencies']
-    fs.writeFileSync('./dist/package.json', JSON.stringify(jsonFile, null, 2))
-  } catch (e) {
-    console.log(e)
-    return false
-  }
-}
-
 // 只读取产线环境变量
 const ENV_FILE = '.env.prod'
 
@@ -22,25 +9,20 @@ const env = config({
 })
 
 if (!env.error) {
-  const envId = env.parsed.TCB_ENVID
   const funcName = env.parsed.TCB_FUNC_NAME
 
-  if (envId && funcName) {
-    const targetDir = `functions/${funcName}`
-
+  if (funcName) {
     await $`rm -rf functions && rm -rf dist`
     await $`ttsc -P tsconfig.json`
-    modifyPackageJson() // 优化 package.json 文件
-    console.log(chalk.green('🎉🎉🎉成功编译代码'))
-    await $`mkdir -p ${targetDir}`
-    await $`cp -r dist/ ${targetDir}`
-    await $`cp index.js .env.prod ${targetDir}`
-    console.log(chalk.green('🎉🎉🎉文件结构梳理完毕'))
+    console.log(chalk.green('🎉🎉🎉 成功编译TS代码'))
+    await $`ncc build ./dist/index.js -s -m -o functions/${funcName}/`
+    await $`cp .env.prod functions/${funcName}/`
+    console.log(chalk.green('🎉🎉🎉 成功缩减JS代码并输出了产线代码'))
     await $`tcb fn deploy --mode prod ${funcName} --force`
-    console.log(chalk.green('🎉🎉🎉成功部署云函数'))
+    console.log(chalk.green('🎉🎉🎉 成功部署云函数'))
   } else {
-    console.log(chalk.red(`请检查 ${ENV_FILE} 内的变量是否填写完整`))
+    console.log(chalk.red(`🤔️🤔️🤔️ 请检查 ${ENV_FILE} 内的变量是否填写完整`))
   }
 } else {
-  console.log(chalk.red('😭😭😭云函数部署失败'))
+  console.log(chalk.red('😭😭😭 云函数部署失败'))
 }
